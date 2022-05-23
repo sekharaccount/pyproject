1dc62080f9e1f25dca4c75e99dfc18d45161bc00
sekharaccount <105232152+sekharaccount@users.noreply.github.com>
2022-05-23 22:04:12 +0530

    Update build.sh

--- src/main/scripts/build.sh
+++ src/main/scripts/build.sh
@@ -1,6 +1,6 @@
 python3 -m venv project1venv
 source project1venv/bin/activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
 pyb 
 deactivate
