f640df006abad074ccc33fea892996d729023630
sekharaccount <105232152+sekharaccount@users.noreply.github.com>
2022-05-23 22:15:11 +0530

    Update build.sh

--- src/main/scripts/build.sh
+++ src/main/scripts/build.sh
@@ -1,7 +1,6 @@
 python3 -m venv project1venv
 source project1venv/bin/activate
 python3 -m pip install --upgrade pip
-pip uninstall pybuilder
 pip install --no-cache-dir -r requirements.txt
 pwd
 pyb 
