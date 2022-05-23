9bfe3b56915d5cc88ff1c60425aa521b4b91ae88
sekharaccount <105232152+sekharaccount@users.noreply.github.com>
2022-05-23 22:40:06 +0530

    Update build.sh

--- src/main/scripts/build.sh
+++ src/main/scripts/build.sh
@@ -4,3 +4,4 @@ python3 -m pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt
 pwd
 pyb 
+zip -r py-project-1.0.zip /var/lib/jenkins/workspace/pythonproject-prod/target/dist/py-project-1.0.dev0
